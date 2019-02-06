6f9d0ffb2e32ec5e4c1a6458c0229cde060362d3
hjain0726 <hjain0726@gmail.com>
2019-02-06 10:44:10 +0530

    new routes added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { CreateComponent } from "./create/create.component";
+import { CountriesComponent } from "./countries/countries.component";
+import { FirstpageComponent } from "./firstpage/firstpage.component";
+import { EditComponent } from "./edit/edit.component";
 
 const appRoutes: Routes = [
-    { path: 'create', component: CreateComponent }
+    { path: 'welcome', component: FirstpageComponent },
+    { path: 'listing', component: CountriesComponent },
+    { path: 'create', component: CreateComponent },
+    { path: 'edit', component: EditComponent },
+    { path: '**', redirectTo: '/welcome' }
 ]
 
 @NgModule({
