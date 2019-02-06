615ec4601e7610202857fe625ea2ad33928dba40
hjain0726 <hjain0726@gmail.com>
2019-02-06 10:45:29 +0530

    new components added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,12 +8,18 @@ import { CreateComponent } from './create/create.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ApiService } from './shared/api.service';
 import { HttpClientModule } from '@angular/common/http';
+import { CountriesComponent } from './countries/countries.component';
+import { FirstpageComponent } from './firstpage/firstpage.component';
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CreateComponent
+    CreateComponent,
+    CountriesComponent,
+    FirstpageComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
