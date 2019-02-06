c8f6ba47544de56f05f79f4a86f551a705096b1a
hjain0726 <hjain0726@gmail.com>
2019-02-06 10:51:19 +0530

    new http requests added

--- frontend/src/app/shared/api.service.ts
+++ frontend/src/app/shared/api.service.ts
@@ -4,9 +4,26 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class ApiService {
+    country;
+    path = "http://localhost:3000/api";
     constructor(private http: HttpClient) { }
 
     saveData(obj: Country) {
-        return this.http.post("http://localhost:3000/api/create", obj);
+        return this.http.post(this.path + "/create", obj);
+    }
+
+    getData() {
+        return this.http.get(this.path + "/givedata");
+    }
+
+    editData(id, obj) {
+        return this.http.put(this.path + "/editdata/" + id, obj);
+    }
+    deletedata(id) {
+        return this.http.delete(this.path + "/deletedata/" + id);
+    }
+
+    setCountry(country) {
+        this.country = country;
     }
 }
