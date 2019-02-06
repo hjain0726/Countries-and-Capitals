84ada0ce747492b18779f3d080e4922b87830397
hjain0726 <hjain0726@gmail.com>
2019-02-06 11:02:30 +0530

    crud operations added

--- backend/db/crud-operations.js
+++ backend/db/crud-operations.js
@@ -6,16 +6,46 @@ const crudOperations = {
         var Obj = new crudModel(obj);
         Obj.save((err) => {
             if (err) {
-                res.send(err);
+                res.json({ msg: "error" });
             } else {
-                var object={
-                    msg:"Successfully Saved"
+                var object = {
+                    msg: "Successfully Saved"
                 };
                 res.json(object);
             }
         })
-    }
+    },
 
+    getData(res) {
+        crudModel.find({}, (err, docs) => {
+            if (err) {
+                res.json({ msg: "error" });
+            } else {
+                if (docs && docs.length > 0) {
+                    res.json(docs);
+                }
+            }
+        })
+    },
+    editData(id, obj, res) {
+        crudModel.findByIdAndUpdate(id, obj, (err) => {
+            if (err) {
+                res.json({ msg: "error" });
+            } else {
+                res.json({ msg: 'Successfully edited' });
+            }
+        })
+    },
+    deleteData(id, res) {
+        crudModel.findByIdAndRemove(id, (err) => {
+            var obj = { msg: 'successfully deleted' }
+            if (err) {
+                res.json({ msg: "error" })
+            } else {
+                res.json(obj);
+            }
+        })
+    }
 };
 
 module.exports = crudOperations;
