113a84c785270a59810c98e0706fe474d137abe3
hjain0726 <hjain0726@gmail.com>
2019-02-06 11:01:42 +0530

    new api request

--- backend/routes/api-routes.js
+++ backend/routes/api-routes.js
@@ -5,6 +5,21 @@ const crudOperations = require('../db/crud-operations');
 router.post('/create', (req, res) => {
   var obj = req.body;
   crudOperations.saveData(obj, res);
+});
+
+router.get('/givedata', (req, res) => {
+  crudOperations.getData(res);
+});
+
+router.put('/editdata/:id', (req, res) => {
+  var obj = req.body;
+  var id = req.params.id;
+  crudOperations.editData(id, obj, res)
+});
+
+router.delete('/deletedata/:id', (req, res) => {
+  var id = req.params.id;
+  crudOperations.deleteData(id, res);
 })
 
 module.exports = router;
